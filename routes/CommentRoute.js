9a9d17d41ec4a7e41a068956ea8abb23a839f46a
Arel Gabay <arelgabay@MacBookPro.lan>
2025-01-02 19:12:48 +0200

    Get Comment By Post Id API

--- routes/CommentRoute.js
+++ routes/CommentRoute.js
@@ -6,5 +6,6 @@ router.post('/', commentController.createComment);
 router.get('/:id', commentController.getCommentById);
 router.put('/:id', commentController.updateComment);
 router.delete('/:id', commentController.deleteComment);
+router.get('/post/:postId', commentController.getCommentsByPost);
 
 module.exports = router;
